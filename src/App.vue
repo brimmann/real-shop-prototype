9ba5dbfd2df64bae20206964fd0b4f01c75f92e3
Mohammad Rashid <rashidstanikzai.net@gmail.com>
2022-04-09 18:29:03 +0500

    moving sample to github

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <signed-header v-if="isLoggedIn" @menu-click="menuClicked"></signed-header>
+  <only-logo-header v-else-if="isNonLogoPath"></only-logo-header>
+  <unsigned-header v-else></unsigned-header>
+
+  <transition name="menu" mode="out-in">
+    <the-menu
+      v-if="isMenuVisible"
+      @option1-clicked="shopManagementClicked"
+      @option2-clicked="isMenuVisible = false"
+      @option3-clicked="isMenuVisible = false"
+      @option4-clicked="isMenuVisible = false"
+      @option5-clicked="logout"
+    ></the-menu>
+  </transition>
+
+  <router-view></router-view>
+  <the-footer></the-footer>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+import TheMenu from "./components/ui/TheMenu";
+import OnlyLogoHeader from "./components/layout/OnlyLogoHeader";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      isMenuVisible: false
+    }
   },
+  components: { OnlyLogoHeader, TheMenu },
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters['user/isLoggedIn'];
+    },
+    isNonLogoPath() {
+      const thisRoute = this.$route;
+      if (thisRoute.path === '/login' || thisRoute.path === '/signup' || thisRoute.path === '/start') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  methods: {
+    shopManagementClicked() {
+      this.isMenuVisible = false;
+      this.$router.push('/management');
+    },
+    menuClicked() {
+      document.addEventListener('scroll', this.documentScrollHandler)
+      this.isMenuVisible = !this.isMenuVisible;
+    },
+    documentScrollHandler() {
+      document.removeEventListener('scroll', this.documentScrollHandler)
+      this.isMenuVisible = false;
+    },
+    logout() {
+      this.isMenuVisible = false
+      this.$store.dispatch('user/logout');
+    }
+  },
+  created() {
+    this.$store.dispatch('user/tryLogin');
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  font-family: Arial;
+  color: black;
 }
+
+body {
+  margin: 0;
+}
+
+img {
+  display: inline-block;
+  max-width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+input,
+textarea {
+  padding: 5px !important;
+}
+
+.menu-enter-from {
+  transform: translateX(70px);
+  opacity: 0;
+}
+.menu-enter-active{
+  transition: all 0.3s ease-in;
+}
+.menu-enter-to,
+.menu-leave-from {
+  transform: translateY(0);
+  opacity: 1;
+}
+.menu-leave-active{
+  transition: all 0.3s ease-in;
+}
+.menu-leave-to{
+  transform: translateX(70px);
+  opacity: 0;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 
-createApp(App).mount("#app");
+
+import App from "./App.vue";
+import store from "@/store";
+import router from "@/router";
+
+
+
+import BaseButton from "@/components/ui/BaseButton";
+import UnsignedHeader from "@/components/layout/UnsignedHeader";
+import SignedHeader from "@/components/layout/SignedHeader";
+import OnlyLogoHeader from "@/components/layout/OnlyLogoHeader";
+import TheFooter from "./components/layout/TheFooter";
+import Paginator from "./components/ui/Paginator";
+import PicturesViewer from "./components/ui/PicturesViewer";
+
+
+
+
+const app = createApp(App);
+
+
+
+app.use(store);
+app.use(router);
+
+
+
+app.component('base-button', BaseButton);
+app.component('unsigned-header', UnsignedHeader);
+app.component('signed-header', SignedHeader);
+app.component('only-logo-header', OnlyLogoHeader);
+app.component('paginator', Paginator);
+app.component('the-footer', TheFooter);
+app.component('pictures-viewer', PicturesViewer);
+
+app.mount("#app");
